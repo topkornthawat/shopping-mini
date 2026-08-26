7148500fe6853eca1b5a8d5c83dca5994d62c369
topkornthawat <kornthawatpornpipat@gmail.com>
2026-08-26 17:08:37 +0700

    Delete src directory

--- src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminChrome from "@/components/AdminChrome";
-
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AdminChrome>{children}</AdminChrome>;
-}
